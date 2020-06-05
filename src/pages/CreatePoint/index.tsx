1053079af357507aaf9880c1ab94f719fdd222ca
Jean Junior <jclgjunior@gmail.com>
2020-06-05 19:01:42 -0300

    Saving data

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
 
@@ -25,10 +26,19 @@ const Home: React.FC = () => {
 		nome: string;
 	}
 
+
 	const [items, setItems] = useState<Item[]>([]);
 	const [ufs, setUfs]= useState<string[]>([]);
 	const [cities, setCities]= useState<string[]>([]);
 	const [selectedUf, setSelectedUf] = useState('0');
+	const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0])
+	const [selectedItems, setSelectedItems] = useState<number[]>([])
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		whatsapp:'',
+	})
+
 
 	useEffect(()=>{
 		api.get('items').then(response => setItems(response.data))
@@ -50,6 +60,31 @@ const Home: React.FC = () => {
 		})
 		
 	},[selectedUf])
+
+	function handleClickMap(e: LeafletMouseEvent){
+		setSelectedPosition([
+			e.latlng.lat,
+			e.latlng.lng
+		])
+	}
+
+	function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+		const { name, value } = event.target;
+		setFormData({ ...formData, [name]:value });
+	}
+
+	function handleSelectItem(id: number){
+		const alreadySelected = selectedItems.findIndex(item => item === id)
+
+		if(alreadySelected >=0){
+			const filteredItems = selectedItems.filter(item => item !== id)
+			setSelectedItems(filteredItems)
+		}
+		else{
+			setSelectedItems([...selectedItems, id])
+		}
+
+	}
 
 	return(
 		<div id="page-create-point">
@@ -73,6 +108,7 @@ const Home: React.FC = () => {
 						type="text"
 						name="name" 
 						id="name"
+						onChange={handleInputChange}
 						/>
 					</div>
 
@@ -83,6 +119,7 @@ const Home: React.FC = () => {
 							type="text"
 							name="email" 
 							id="email"
+							onChange={handleInputChange}
 							/>
 						</div>
 						<div className="field">
@@ -91,6 +128,7 @@ const Home: React.FC = () => {
 							type="text"
 							name="whatsapp" 
 							id="whatsapp"
+							onChange={handleInputChange}
 							/>
 						</div>
 					</div>
@@ -102,13 +140,13 @@ const Home: React.FC = () => {
 						<span>Selecione um endereço no mapa</span>
 					</legend>
 
-					<Map center={[-4.466322,-43.8768724]} zoom={15}>
+					<Map center={[-4.466322,-43.8768724]} zoom={15} onClick={handleClickMap}>
 						<TileLayer 
 							attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 						/>
 
-						<Marker position={[-4.466322,-43.8768724]} />
+						<Marker position={selectedPosition} />
 					</Map>
 
 					<div className="field-group">
@@ -139,7 +177,11 @@ const Home: React.FC = () => {
 
 					<ul className="items-grid">
 					{items.map(item => (
-							<li key={item.id} className='selected'>
+							<li 
+							key={item.id} 
+							onClick={()=>handleSelectItem(item.id)}
+							className={selectedItems.includes(item.id) ? 'selected' : ''}
+							>
 								<img src={item.image_url} alt="Teste"/>
 								<span>{item.title}</span>
 						</li>
